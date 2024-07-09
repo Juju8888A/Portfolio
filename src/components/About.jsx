4c7fae8b8c63c41310ff801faa7a549c735d6c4b
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-07-09 16:30:27 +0200

    structuration des sections a propos et mes compétences

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <section id="about" className='section-container'>
+        <section id="about">
             <h2>A propos</h2>
                 <p>Animatrice 3D depuis 2015, je me suis reconvertie dans le développement web et spécialisée en React. Ayant de base des compétences techniques, plus un bagage créatif, je suis ravie aujourd'hui de pouvoir apporter mes compétences en développement et artistiques, dans le but de réaliser des projets innovants et de créer des interfaces utilisateurs modernes et qualitatives. </p>
         </section>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,11 +10,14 @@ const Skills = () => {
 
 
     return (
-        <section className='section-container' id="skills">
+        <section id="skills">
+            
             <h2>Mes compétences</h2>
+            <div className='skills-cards-container'>
             <Skill title="Développement front-end" description="Réalisation d'interface utilisateur" otherTitle="Logiciels, langages utilisés" skills={frontendSkills}/>
             <Skill title="Développement back-end" description="Création d'un serveur, sécurisation des données" otherTitle="Logiciels, langages utilisés" skills={backendSkills}/>
             <Skill title="Animation 3D" description="Animation des personnages" otherTitle="Logiciels, langages utilisés" skills={animationSkills}/>
+            </div>
         </section>
     );
 };
