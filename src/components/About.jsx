302d2776fac71c64d7c39d1213916b9fb1943f11
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-07-11 13:44:15 +0200

    restructuration de la section a propos, ajout des liens github et linkedin

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   return (
     <section id="about" className="about-section">
       <h2>A propos</h2>
-
-      <p>
-        Animatrice 3D depuis 2015, je me suis reconvertie dans le développement
-        web, spécialisée en front-end. Ayant de base des compétences techniques,
-        plus un bagage créatif, je suis ravie aujourd'hui de pouvoir apporter
-        mes compétences en développement et artistiques, dans le but de réaliser
-        des projets innovants et de créer des interfaces utilisateurs modernes
-        et qualitatives.{" "}
-      </p>
-      <img
-        src="./assets/images/portrait.webp"
-        alt="portrait de juliette jourdan"
-      />
+      <div class="container-about">
+        <div class="texte-liens">
+          <p>
+            Animatrice 3D depuis 2015, je me suis reconvertie dans le
+            développement web, spécialisée en front-end. Ayant de base des
+            compétences techniques, plus un bagage créatif, je suis ravie
+            aujourd'hui de pouvoir apporter mes compétences en développement et
+            artistiques, dans le but de réaliser des projets innovants et de
+            créer des interfaces utilisateurs modernes et qualitatives.{" "}
+          </p>
+          <div class="liens-externes">
+          <a href="https://github.com/Juju8888A" target="_blank" rel="noopener noreferrer" id="lien-github"><FontAwesomeIcon icon={faGithub}/></a>
+          <a href="https://www.linkedin.com/in/juliettejourdan" target="_blank" rel="noopener noreferrer"id="lien-linkedin"><FontAwesomeIcon icon={faLinkedin} /></a>
+          </div>
+        </div>
+        <div class="profil">
+          <img
+            src="./assets/images/portrait.webp"
+            alt="portrait de juliette jourdan"
+          />
+        </div>
+      </div>
     </section>
   );
 };
