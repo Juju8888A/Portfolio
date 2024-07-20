8ea010aa016ad8be46f94f8563bebf63fa24ac3e
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-07-20 21:07:51 +0200

    modif fond pas totalement blanc + amélioration responsive design version mobile + logo en webp

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Logo = () => {
     return (
         <div className='logo-container'>
-            <NavLink to="/"><img src="/Portfolio/assets/images/logo.png" alt="logo du portfolio de juliette jourdan"/></NavLink>
+            <NavLink to="/"><img src="/Portfolio/assets/images/logo.webp" alt="logo du portfolio de juliette jourdan"/></NavLink>
         </div>
     );
 };
