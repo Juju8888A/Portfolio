55ae4b2cd5027bbd40ae18157bf0407648b5f1d4
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-10-04 14:10:36 +0200

    mise à jour cv

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { NavLink } from "react-router-dom";
 
-const Skill = ( {icone, title, description, otherTitle, skills, link} ) => {
-    return (
-        <div className="skills-card">
-            <span><FontAwesomeIcon icon={icone} /></span>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <h4>{otherTitle}</h4>
-            <ul>
-                {skills.map((skill, index) => (
-                    <li key={index}>{skill}</li>
-                ))}
-            </ul>
-            {link && (
-                <a href={link} target="_blank" id='link-demo'>DEMO</a>
-            )}      
-        </div>
-    );
+const Skill = ({ icone, title, description, otherTitle, skills, link }) => {
+  return (
+    <div className="skills-card">
+      <span>
+        <FontAwesomeIcon icon={icone} />
+      </span>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <h4>{otherTitle}</h4>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+      {link && (
+        <a href={link} target="_blank" id="link-demo">
+          DEMO
+        </a>
+      )}
+    </div>
+  );
 };
 
 export default Skill;
